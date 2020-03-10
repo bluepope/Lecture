5281303d56d0ae79269eff647b47fcdee809ee28
안현모 <blue_pope@hotmail.com>
2020-03-10 11:16:01 +0900

    초기 커밋.

---
+++ Lecture.DapperDemo2/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Lecture.DapperDemo2.Models;
+using System.Collections.ObjectModel;
+
+namespace Lecture.DapperDemo2
+{
+    public partial class Form1 : Form
+    {
+        BindingList<BoardModel> _list = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            dataGridView1.CellValueChanged += DataGridView1_CellValueChanged;
+        }
+
+        private void DataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            var item = (sender as DataGridView).Rows[e.RowIndex].DataBoundItem as BoardModel;
+            if ((item.isNew || item.isEdit) == false)
+                item.isEdit = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _list = new BindingList<BoardModel>(BoardModel.GetList(""));
+            dataGridView1.DataSource = _list;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            foreach(var item in _list)
+            {
+                //if (item.isNew) item.Insert();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            _list.Add(new BoardModel() { isNew = true });
+        }
+    }
+}
---
+++ Lecture.DapperDemo2/Models/BoardModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Lecture.Lib.Database;
+
+namespace Lecture.DapperDemo2.Models
+{
+    public class BoardModel : INotifyPropertyChanged
+    {
+        bool _isNew = false;
+        public bool isNew { get => _isNew; set => SetProperty(value); }
+
+        bool _isEdit = false;
+        public bool isEdit { get => _isEdit; set => SetProperty(value); }
+        public bool isDelete { get; set; }
+
+        public decimal SEQ { get; set; }
+        public string TITLE { get; set; }
+        public string CONTENTS { get; set; }
+        public decimal REG_U_ID { get; set; }
+        public string REG_NAME { get; set; }
+        public DateTime REG_DATE { get; set; }
+        public string STATUS { get; set; }
+        public DateTime? UPDATE_DATE { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual bool SetProperty(object value, [CallerMemberName] string propertyName = null)
+        {
+            this.GetType().GetField($"_{propertyName}", BindingFlags.NonPublic | BindingFlags.Instance).SetValue(this, value);
+            OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected virtual bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (Equals(storage, value))
+            {
+                return false;
+            }
+
+            storage = value;
+            OnPropertyChanged(propertyName);
+
+            return true;
+        }
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public static List<BoardModel> GetList(string search)
+        {
+            return SqlDapperHelper.Instance.Query<BoardModel>("SELECT * FROM T_BOARD", new { search = search });
+        }
+    }
+}
