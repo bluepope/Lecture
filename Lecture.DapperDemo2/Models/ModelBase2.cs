27d56f322438ad7455a96f66ea161c7ef544ad97
안현모 <blue_pope@hotmail.com>
2020-03-10 15:07:26 +0900

    dispose 추가

--- Lecture.DapperDemo2/Models/ModelBase2.cs
+++ Lecture.DapperDemo2/Models/ModelBase2.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 
 namespace Lecture.DapperDemo2.Models
 {
-    public class ModelBase2 : INotifyPropertyChanged
+    public class ModelBase2 : INotifyPropertyChanged, IDisposable
     {
         public event PropertyChangedEventHandler PropertyChanged;
         public Dictionary<string, object> _storage = new Dictionary<string, object>();
@@ -36,5 +36,42 @@ namespace Lecture.DapperDemo2.Models
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
         }
+
+        #region IDisposable Support
+        private bool disposedValue = false; // 중복 호출을 검색하려면
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    _storage.Clear();
+                }
+
+                // TODO: 관리되지 않는 리소스(관리되지 않는 개체)를 해제하고 아래의 종료자를 재정의합니다.
+                // TODO: 큰 필드를 null로 설정합니다.
+                _storage = null;
+
+                disposedValue = true;
+            }
+        }
+
+        // TODO: 위의 Dispose(bool disposing)에 관리되지 않는 리소스를 해제하는 코드가 포함되어 있는 경우에만 종료자를 재정의합니다.
+        // ~ModelBase2()
+        // {
+        //   // 이 코드를 변경하지 마세요. 위의 Dispose(bool disposing)에 정리 코드를 입력하세요.
+        //   Dispose(false);
+        // }
+
+        // 삭제 가능한 패턴을 올바르게 구현하기 위해 추가된 코드입니다.
+        public void Dispose()
+        {
+            // 이 코드를 변경하지 마세요. 위의 Dispose(bool disposing)에 정리 코드를 입력하세요.
+            Dispose(true);
+            // TODO: 위의 종료자가 재정의된 경우 다음 코드 줄의 주석 처리를 제거합니다.
+            // GC.SuppressFinalize(this);
+        }
+        #endregion
     }
 }
