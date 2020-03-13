e23218af502e5d160361ad5b7f1350671e352f60
안현모 <blue_pope@hotmail.com>
2020-03-13 23:39:25 +0900

    dapper demo 1과 병합

---
+++ Lecture.DapperDemo1/Models/UserModel.cs
@@ -0,0 +1,54 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace Lecture.DapperDemo1.Models
+{
+    public class UserModel
+    {
+        public long U_SEQ { get; set; }
+        public string U_ID { get; set; }
+        public string U_NAME { get; set; }
+        public string U_PW { get; set; }
+        public DateTime JOIN_DATE { get; set; }
+
+        public static List<UserModel> GetList(IDbConnection db, string search)
+        {
+            string sql = @"
+SELECT
+    U_SEQ
+    ,U_ID
+    ,U_NAME
+    ,U_PW
+    ,JOIN_DATE
+FROM
+    T_USER
+WHERE
+    U_NAME LIKE @search + '%'";
+
+            return SqlMapper.Query<UserModel>(db, sql, new { search = "" }).ToList();
+        }
+
+        public int Insert(IDbConnection db, IDbTransaction trans)
+        {
+            string insertSql = @"
+INSERT INTO T_USER(
+    U_ID
+    ,U_NAME
+    ,U_PW
+    ,JOIN_DATE
+)
+VALUES(
+    @U_ID
+    ,@U_NAME
+    ,@U_PW
+    ,@JOIN_DATE
+)";
+
+            return SqlMapper.Execute(db, insertSql, this, trans);
+        }
+    }
+}
---
+++ Lecture.DapperDemo1/Program.cs
@@ -0,0 +1,84 @@
+﻿using Dapper;
+using Lecture.DapperDemo1.Models;
+using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace Lecture.DapperDemo1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            #region DataTable 예제
+            var dt = new DataTable();
+
+            using (var db = new SqlConnection("Server=192.168.0.200;Database=lecture;User Id=study;Password=study2020!!;"))
+            {
+                db.Open();
+
+                using (var cmd = new SqlCommand())
+                {
+                    cmd.Connection = db;
+                    cmd.CommandText = insertSql;
+                    cmd.Parameters.AddWithValue("U_ID", "redpope");
+                    cmd.Parameters.AddWithValue("U_NAME", "레드");
+                    cmd.Parameters.AddWithValue("U_PW", "4321");
+                    cmd.Parameters.AddWithValue("JOIN_DATE", DateTime.Now);
+                    cmd.ExecuteNonQuery();
+                }
+
+                using (var cmd = new SqlCommand())
+                {
+                    cmd.Connection = db;
+                    cmd.CommandText = sql;
+                    cmd.Parameters.AddWithValue("search", "레");
+
+                    dt.Load(cmd.ExecuteReader());
+                }
+            }
+
+            foreach (DataRow row in dt.Rows)
+            {
+                Console.WriteLine(row["U_SEQ"] as long?);
+                Console.WriteLine(row["U_NAME"] as string);
+            }
+            #endregion
+            */
+
+            using (var db = new SqlConnection("Server=192.168.0.200;Database=lecture;User Id=study;Password=study2020!!;"))
+            {
+                db.Open();
+
+                var tran = db.BeginTransaction();
+
+                try
+                {
+                    var model = new UserModel();
+                    model.U_ID = "white";
+                    model.U_NAME = "화이트";
+                    model.U_PW = "555";
+                    model.JOIN_DATE = DateTime.Now;
+                    model.Insert(db, tran);
+
+                    tran.Commit();
+                }
+                catch(Exception ex)
+                {
+                    tran.Rollback();
+                    Console.WriteLine(ex.Message);
+                }
+
+                var list = UserModel.GetList(db, "");
+
+                foreach (var item in list)
+                {
+                    Console.WriteLine($"{item.U_ID} {item.U_NAME}");
+                }
+
+            }
+        }
+    }
+}
